d01c9b18fa372ee2d08954fd9e75f4506a593090
lacykaltgr <freundl0509@gmail.com>
2025-03-05 01:01:55 +0100

    add livekit server

---
+++ controller/web-server/livekit_server/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import { AccessToken } from 'livekit-server-sdk';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.post('/createToken', async (req, res) => {
+  const { participantName = 'quickstart-username', roomName = 'quickstart-room' } = req.body;
+  console.log("Creating token for " + participantName + " at " + roomName );
+  try {
+    const at = new AccessToken(process.env.LIVEKIT_API_KEY, process.env.LIVEKIT_API_SECRET, {
+      identity: participantName,
+      ttl: 6000, // 10 minutes in seconds
+    });
+
+    at.addGrant({ roomJoin: true, room: roomName });
+
+    const token = await at.toJwt();
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ error: 'Error generating token', details: error.message });
+  }
+});
+
+const PORT = 8041;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
